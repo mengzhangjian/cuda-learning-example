f32ab0b0db09262332df7fc5ecc0bbffa0d18ffb
zhangjian49 <zhangjian49@baidu.com>
2019-06-06 16:22:19 +0800

    v2

--- ch05/ripple.cu
+++ ch05/ripple.cu
@@ -2,4 +2,51 @@
 #include "../common/book.h"
 #include "../common/image.h"
 
+const int DIM(1024);
+const float PI(3.1415926535897932);
 
+__global__ void kernel(unsigned char *ptr, int ticks){
+
+    //map from threadIdx / BlockIdx to pixel position
+
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int offset = x + y * blockDim.x * gridDim.x;
+
+    //calculate the value at that position
+    float fx = x - DIM/2;
+    float fy = y - DIM/2;
+    float d = sqrtf( fx * fx + fy * fy );
+    unsigned char grey = (unsigned char)(128.0f + 127.0f *
+                                                  cos(d/10.0f - ticks/7.0f) /
+                                                  (d/10.0f + 1.0f));
+    ptr[offset*4 + 0] = grey;
+    ptr[offset*4 + 1] = grey;
+    ptr[offset*4 + 2] = grey;
+    ptr[offset*4 + 3] = 255;
+}
+
+struct  DataBlock{
+    unsigned  char *dev_bitmap;
+    IMAGE *bitmap;
+};
+
+void cleanUp( DataBlock *d){
+    cudaFree(d->dev_bitmap);
+}
+
+int main(void){
+    DataBlock data;
+    IMAGE bitmap(DIM, DIM);
+    data.bitmap = &bitmap;
+
+    cudaMalloc((void**) &data.dev_bitmap, bitmap.image_size());
+
+    dim3 blocks(DIM / 16, DIM / 16);
+    dim3 threads(16, 16);
+
+    int tricks = 0;
+    bitmap.show_image(30);
+
+
+}
