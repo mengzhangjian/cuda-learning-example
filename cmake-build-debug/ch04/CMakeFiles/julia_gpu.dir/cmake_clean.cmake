ac5d4868023d6b628c2497d5f43e8f5bc68d0cc9
mengzhangjian <mengzhangjian@gmail.com>
2019-06-18 00:29:51 +0800

    rm

---
+++ cmake-build-debug/ch04/CMakeFiles/julia_gpu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/julia_gpu.dir/julia_gpu_generated_julia_gpu.cu.o"
+  "../bin/julia_gpu.pdb"
+  "../bin/julia_gpu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/julia_gpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
